27590dbec1dcb66657cc216d04d49181b2527673
JonasGredig <jongredig@gmail.com>
2018-02-08 10:21:32 +0100

    implemented ClickController

--- src/ch/baumink/buttonclicker/controller/ClickController.java
+++ src/ch/baumink/buttonclicker/controller/ClickController.java
@@ -1,4 +1,20 @@
 package ch.baumink.buttonclicker.controller;
 
+import ch.baumink.buttonclicker.model.ClickModel;
 public class ClickController {
+
+    private ClickModel clickModel;
+
+    public ClickController() {
+        clickModel = new ClickModel();
+    }
+
+    public long getAmount() {
+        return clickModel.getAmount();
+    }
+
+    public boolean increaseAmount() {
+        clickModel.setNumber(clickModel.getAmount() + 1);
+        return true;
+    }
 }
